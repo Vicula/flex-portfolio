859219548841cc8c42022d973ca9d77850244c03
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-23 09:33:09 -0500

    got it to do things

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ var element = document.querySelector('.app-container');
 
 var panel = document.querySelector('.panel-3');
 var panelShadow = document.querySelector('.shadow-current')
+var allPanel = document.querySelectorAll('.panel')
+
+var isOn = false;
 
 var panelWidth = panel.offsetWidth
 var panelHeight = panel.offsetHeight
@@ -35,11 +38,37 @@ var mouseFunc = function(e){
 
 }
 
+var tileOn = function(e){
+  for (let i = 0; i < allPanel.length; i++){
+    let newName = allPanel[i].classList[0] + " " + allPanel[i].classList[1]
+    // console.log(allPanel[i].children)
+    let shaName = allPanel[i].children[0].classList[0] + " " + allPanel[i].children[0].classList[1]
+    // console.log(shaName)
+    allPanel[i].children[0].className = shaName
+    allPanel[i].className = newName
 
-var tileMove = function(e){
-  
+  }
+}
+var tileOff = function(e){
+
+}
+
+
+var statusSwitch = function(e){
+
+  if(isOn === false){
+    tileOn(e)
+    isOn = true
+  } else {
+    tileOff(e)
+    isOn = false
+
+  }
+  // console.log(e)
+  //
+  // console.log(allPanel)
 }
 
 
 
-// panel.addEventListener('mousemove', mouseFunc)
+panel.addEventListener('click', statusSwitch)
