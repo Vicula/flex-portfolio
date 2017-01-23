e31af8f22675c6d76b9c17348577a3109507f612
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-23 08:52:59 -0500

    starting on the first transition stuff

--- js/app.js
+++ js/app.js
@@ -13,12 +13,21 @@ var mouseFunc = function(e){
 
   // console.log(e)s
 
+    // panel.style.transform = "skew(0deg, -3deg)rotate3d(0,1,0,-3deg)"
+    // panelShadow.style.background = "linear-gradient(to right, rgba(45,45,45,0.4) 5%, rgba(0,0,0,0) 60%)"
 
-  panel.style.transform = "rotate3d(0,1,0,-25deg)"
+    // console.log("X =" +(e.offsetX/panelWidth)*100);
+    // console.log("Y =" +(e.offsetY/panelHeight)*100);
 
-  console.log("X =" +(e.offsetX/panelWidth)*100);
-  console.log("Y =" +(e.offsetY/panelHeight)*100);
 
+  //
+  // let xPos = (e.offsetX/panelWidth)*100
+  // // let yPos =
+  // // console.log(-3/xPos)
+  // if(xPos > 50){
+  //   xPos = xPos - 50
+  //   console.log(xPos *0.5)
+  // }
 
   // let xPos = (e.screenX/windowWidth)*100
 
@@ -27,5 +36,10 @@ var mouseFunc = function(e){
 }
 
 
+var tileMove = function(e){
+  
+}
 
-panel.addEventListener('mousemove', mouseFunc)
+
+
+// panel.addEventListener('mousemove', mouseFunc)
